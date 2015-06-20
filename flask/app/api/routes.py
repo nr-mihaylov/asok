a2cc36ab4d9c19319918a06b4846b76b42c0054a
Justas Azna <ja@fadeit.dk>
2015-06-20 15:27:54 +0200

    Add frontend template and polish

--- flask/app/api/routes.py
+++ flask/app/api/routes.py
@@ -1,6 +1,18 @@
 # import blueprint
 from . import api
+from flask import jsonify, request
+
 
 @api.route('/')
 def index():
-    return 'hello world'
+    return jsonify(data='hello there')
+
+
+@api.route('/locations', methods=['GET', 'POST'])
+def locations():
+    if request.method == 'GET':
+        # TODO: GET- retrieve locations from sqlite
+        return jsonify(locations=[])
+    else:
+        # TODO: POST- save location to sqlite
+        return jsonify(status='OK')
--- flask/app/frontend/routes.py
+++ flask/app/frontend/routes.py
@@ -1,11 +1,13 @@
 from . import frontend
+from flask import render_template
 
 
 @frontend.app_errorhandler(404)
 def page_not_found(e):
-    return 'We could not find this one', 404
+    return 'Page not found', 404
 
 
+# Render HTML
 @frontend.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html')
