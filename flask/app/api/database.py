9badd684af9b43d1e55b890c42fa852f63bbf281
Nikolay Rumenov Mihaylov <nr.mihaylov@gmail.com>
2016-11-08 00:34:14 +0100

    switch to memory db

--- flask/app/api/database.py
+++ flask/app/api/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Float, String
 
-engine = create_engine('sqlite:///D:\\Projects\\asok\\lstorage.db', convert_unicode=True)
+engine = create_engine('sqlite:////lstorage.db', convert_unicode=True)
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = session.query_property()
